70870da49147b3ecf8245f8cc6a3076f49103411
SujoodEldda <sujood.eldda@gmail.com>
2024-01-15 14:40:48 +0200

    changing names

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser')
 const api = require('./server/routes/api')
 
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mydb', { useNewUrlParser: true })
+mongoose.connect('mongodb://localhost/uni-gathering', { useNewUrlParser: true })
 
 const app = express()
 
