fa5cc118bf9294eba74b77e1e1d2d0ddac9862f9
ReemSh <reem.shamasnah@gmail.com>
2024-01-16 14:09:31 +0200

    add get my activities route

--- server/routes/activitiesApi.js
+++ server/routes/activitiesApi.js
@@ -1,56 +1,43 @@
 const express = require('express')
 const router = express.Router()
-const Item = require('../models/Item')
-const dbManager = require('./server/DBManager')
+const dbManager = require('../DBManager')
 const User = require('../models/user')
 const Activity = require('../models/activity')
 
 
 
-router.get('/', async function(req, res){
+router.get('/myActivities/:userId', async function(req, res) {
+    try {
+        const userId = req.params.userId
+        const activities = await Activity.find({userId : userId})
+        res.send(activities)
+        
+    } catch (error) {
+        res.status(400).send(error)
+    }
+});
+
+router.post('/:userId', async function(req, res){
+    try{
+    const userId = req.params.userId
+    const userUniversityName = await User.findOne({'_id' : userId},{universityName :1 })
+    const saveMeActivity = {...req.body ,userUniversityName }
+    const newActivity = new Activity(saveMeActivity)
+    newActivity.save()
+    res.status(200).end()
+    }catch (error) {
+        res.status(400).send(error)
+    }
+})
+
+router.get('/DBgenerator', async function(req, res){
     try{
         await dbManager.reGenerate()
         res.end()
     }catch(err){
         console.error(err);
-        throw new Error(`Failed to generate data`);
         res.status(400).send(err => err)
     }
 
 })
-
-router.get('/get', (req, res) => {
-    Item.find({}, function(err, data) {
-        console.log(data)
-        res.send(data)
-    })
-});
-
-router.post('/:userId', async function(req, res){
-    try{
-    let activity = req.body
-
-    const userId = req.params.userId
-    const userUniversityName = await User.find({_id : userId},{universityName :1 , _id :0})
-
-    const saveMeActivity = {}
-    saveMeActivity.userID = userId
-    saveMeActivity.universityName = userUniversityName[0].universityName
-    saveMeActivity.date = activity.date
-    saveMeActivity.location = activity.location 
-    saveMeActivity.transportationType = activity.transportationType 
-    saveMeActivity.capacity = activity.capacity
-    saveMeActivity.activityType = activity.activityType
-    saveMeActivity.preferredGender = activity.gender 
-    saveMeActivity.participants = []
-
-    let newActivity = new Activity(saveMeActivity)
-    newActivity.save()
-    res.status(200).end()
-    }catch(err){
-        console.error(err)
-        res.status(400).send(err)
-    }
-})
-
 module.exports = router
