6c8370768a2f212ac2fbe18343b7e086d42fc6d5
SujoodEldda <sujood.eldda@gmail.com>
2024-01-16 11:38:09 +0200

    Revert "Revert "adding the user and university and activity schemas and adding the generate and regenerate functions""
    This reverts commit b86d338701a2de7ef2bdab33d0be24718682f98c.

--- server/DBManager.js
+++ server/DBManager.js
@@ -1,6 +1,36 @@
-class DBManager{
-    connectToDB(){
-        mongoose.connect("mongodb://127.0.0.1:27017/Amani-DB")
-            .catch((err)=> console.log(err))
+const mongoose = require('mongoose')
+const fs = require("fs")
+const User = require('./models/user')
+const University = require('./models/university')
+const Activity = require('./models/activity')
+const activities = JSON.parse(fs.readFileSync("../activity.json"))
+const universities = JSON.parse(fs.readFileSync("../university.json"))
+const users = JSON.parse(fs.readFileSync("../user.json"))
+
+class DBServer{
+    static connectToDB(){
+        mongoose.connect('mongodb://localhost/Amani-DB', { useNewUrlParser: true })
+    }
+    static async generateData(){
+        universities.forEach(async uni => {
+            const newUniversity = new University(uni)
+            await newUniversity.save()
+        })
+        activities.forEach(async activity => {
+            const newActivity = new Activity(activity)
+            await newActivity.save()
+        })
+        users.forEach(async user => {
+            const newUser = new User(user)
+            await newUser.save()
+        })
+    }
+    static async reGenerate(){
+        await User.deleteMany({})
+        await Activity.deleteMany({})
+        await University.deleteMany({})
+        DBServer.generateData()
     }
 }
+
+module.exports = DBServer
