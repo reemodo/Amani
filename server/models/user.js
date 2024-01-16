d65db2ccd54c327be75b41f4a4e7a5f09072fd98
andalusm <37974151+andalusm@users.noreply.github.com>
2024-01-16 13:06:21 +0200

    Revert "Db branch"

---
+++ server/models/user.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const UserSchema = new Schema({
+    _id: String,
+    email: String,
+    password: String,
+    gender: String, 
+    phoneNumber: String,
+    name: String,
+    location: String,
+    universityName: String
+})
+
+const user = mongoose.model("user", UserSchema)
+module.exports = user
