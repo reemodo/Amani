e25ad150929f9ee6976f56f430de13f3ab4f4520
nadeen-maree <nadeen.maree1610@gmail.com>
2024-01-16 10:34:45 +0200

    addActivity method added to class ActivityController class

--- dist/models/ActivityController.js
+++ dist/models/ActivityController.js
@@ -1,2 +1,13 @@
 const activityModel = new ActivityModel()
-const renderer = new Renderer()
+const renderer = new Renderer() 
+
+class ActivityController {
+    constructor(model, view) {
+        this.model = model
+        this.view = view
+    }
+
+    addActivity(){
+        
+    }
+}
