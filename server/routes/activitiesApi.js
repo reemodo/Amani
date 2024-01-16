1956ea23f4eaef95df342142860ca2c0eeeea0f7
andalusm <andalus0220@gmail.com>
2024-01-16 12:50:45 +0200

    Fixed bugs

--- server/routes/activitiesApi.js
+++ server/routes/activitiesApi.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const Item = require('../models/Item')
-const dbManager = require('./server/DBManager')
+const dbManager = require('../DBManager')
 
 
 router.get('/', async function(req, res){
