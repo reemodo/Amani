e215cb78c54115a62d7e9662bae2f561bcaae150
Your username <noor-nakhle@hotmail.com>
2024-01-17 11:42:13 +0200

    added headers

--- dist/models/ActivityModel.js
+++ dist/models/ActivityModel.js
@@ -3,10 +3,36 @@ class ActivityModel {
         try {
             
           if(filtered){
-            return await $.get(`/activities/${userId}?${$.param(filterData)}` )
+            //return await $.get(`/activities/${userId}?${$.param(filterData)}` )
+            return await $.ajax({
+                url: `/activities/${userId}?${$.param(filterData)}`,
+                type: 'GET',
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem('token')}`
+                  },
+                success: function() {
+                   return true
+                },              
+                error: function(xhr, status, error) {
+                    console.error(error)
+                }
+            })
           }
           else {
-             return await $.get(`/activities/${userId}`)
+             //return await $.get(`/activities/${userId}`)
+             return await $.ajax({
+                url: `/activities/${userId}`,
+                type: 'GET',
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem('token')}`
+                },
+                success: function() {
+                   return true
+                },              
+                error: function(xhr, status, error) {
+                    console.error(error)
+                }
+            })
           }
         } catch (error) {
           throw error
@@ -15,7 +41,23 @@ class ActivityModel {
 
     async addActivity(userId, activityData) {
         try {
-            await $.post(`/activities/${userId}`, activityData)
+           // await $.post(`/activities/${userId}`, activityData)
+            await $.ajax({
+            url: `/activities/${userId}`,
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(activityData),
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem('token')}`
+            },
+            success: function(response) {
+                alert("Your activity has successfully added.")
+              },
+              error: function(error) {
+                alert("Couldn't add the activity.")
+              }
+          })
+           
         } catch (error) {
             throw error
         }
@@ -23,7 +65,20 @@ class ActivityModel {
 
     async showMyActivities(userId) {
         try {
-            const data = await $.get(`/activities/myActivities/${userId}`)
+            // const data = await $.get(`/activities/myActivities/${userId}`)
+            const data = await $.ajax({
+                url: `/activities/${userId}`,
+                type: 'GET',
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem('token')}`
+                },
+                success: function() {
+                   return true
+                },              
+                error: function(xhr, status, error) {
+                    console.error(error)
+                }
+            })
             return data
         } catch (error) {
             throw error
